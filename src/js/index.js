3b24c59c853c4e792f45a013aab3187c637179f9
yabsiraditya <yabsir.aditya@gmail.com>
2023-12-02 22:57:23 +0700

    Add File Portofolio

---
+++ src/js/index.js
@@ -0,0 +1,31 @@
+function myFunction() {
+  // Typing Js
+  new Typed('#skill', {
+    strings: [
+      'Web Developer...',
+      'Grapich Designer...',
+    ],
+    typeSpeed: 80,
+    backSpeed: 30,
+    backDelay: 1800,
+    loop: true,
+    loopCount: Infinity,
+  });
+}
+
+// Toggle Dark Mode
+function darkModeIcon() {
+  const btn = document.getElementById('darkmode')
+    let theme = document.documentElement.getAttribute('data-bs-theme');
+    if (theme === 'dark') {
+      setTimeout(() => {
+        document.documentElement.removeAttribute('data-bs-theme');
+        btn.innerHTML = `<i class="fa-solid fa-moon fa-lg me-2"></i>Dark`;
+      }, 180);
+    } else {
+      setTimeout(() => {
+        document.documentElement.setAttribute('data-bs-theme', 'dark');
+        btn.innerHTML = `<i class="fa-solid fa-sun fa-lg me-2"></i>Light`;
+      }, 180);
+    }
+} 
